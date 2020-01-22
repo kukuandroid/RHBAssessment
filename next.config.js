c8e1355c996641a611c7d922c12a024d0f2ce52d
KhairulFilhan <khairulfilhan94@gmail.com>
2020-01-23 03:44:27 +0800

    .env file

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    API_URL: 'https://api.github.com/search/repositories',
+  },
+};
--- services/ApiServices.js
+++ services/ApiServices.js
@@ -1,5 +1,4 @@
 
-const API_URL = 'https://api.github.com/search/repositories';
 const axios = require('axios');
 
-export const getRepositories = (pageNo, perPage, q) => axios.get(`${API_URL}?per_page=${perPage || 10};q=${q || null};page=${pageNo}`);
+export const getRepositories = (pageNo, perPage, q) => axios.get(`${process.env.API_URL}?per_page=${perPage || 10};q=${q || null};page=${pageNo}`);
